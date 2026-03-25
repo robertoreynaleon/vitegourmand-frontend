885998697088652247811b78e4c527f31c1614ca
rreynaleon <ro.reyna83@gmail.com>
2026-03-25 11:36:59 +0100

    Création de la route et page MenuList

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
+import MenuList from './pages/menu/MenuList';
 import './App.scss';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<HomePage />} />
+        <Route path='/menu/list/' element={<MenuList />} />
         
       </Routes>
     </BrowserRouter>
